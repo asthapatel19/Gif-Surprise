0a2efe20562436f361f420792c4ed0092c772157
asthapatel19 <astha.p@ahduni.edu.in>
2023-03-29 10:30:05 +0530

    react hooks

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className='w-full h-screen flex flex-col background relative overflow-x-hidden item-center'>
         
-        <h1 className='bg-white rounded-lg w-11/12 text-center mt-[40px] ml-[55px] px-10 py-2 text-3xl font-bold'>Random Gif's Generator</h1>
+        <h1 className='bg-white rounded-lg w-11/12 text-center mt-[40px] ml-[55px] px-10 py-2 text-3xl font-bold'>GIF SURPRISE</h1>
         
         <div className="flex flex-col w-full items-center gap-y-10 mt-[30px]">
           <Random/>
